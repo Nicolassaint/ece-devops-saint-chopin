9a538fc1d37105740081bb6d2eb0b6dcac434851
nicolassaint <nicolas.saint78@gmail.com>
2022-10-11 15:06:08 +0200

    feat: lab3

--- test/user.controller.js
+++ test/user.controller.js
@@ -1,71 +1,92 @@
-const { expect } = require('chai')
-const userController = require('../src/controllers/user')
-const db = require('../src/dbClient')
+const { expect } = require("chai");
+const userController = require("../src/controllers/user");
+const db = require("../src/dbClient");
 
-describe('User', () => {
-  
+describe("User", () => {
   beforeEach(() => {
     // Clean DB before each test
-    db.flushdb()
-  })
+    db.flushdb();
+  });
 
-  describe('Create', () => {
-
-    it('create a new user', (done) => {
+  describe("Create", () => {
+    it("create a new user", (done) => {
       const user = {
-        username: 'sergkudinov',
-        firstname: 'Sergei',
-        lastname: 'Kudinov'
-      }
+        username: "sergkudinov",
+        firstname: "Sergei",
+        lastname: "Kudinov",
+      };
       userController.create(user, (err, result) => {
-        expect(err).to.be.equal(null)
-        expect(result).to.be.equal('OK')
-        done()
-      })
-    })
+        expect(err).to.be.equal(null);
+        expect(result).to.be.equal("OK");
+        done();
+      });
+    });
 
-    it('passing wrong user parameters', (done) => {
+    it("passing wrong user parameters", (done) => {
       const user = {
-        firstname: 'Sergei',
-        lastname: 'Kudinov'
-      }
+        firstname: "Sergei",
+        lastname: "Kudinov",
+      };
       userController.create(user, (err, result) => {
-        expect(err).to.not.be.equal(null)
-        expect(result).to.be.equal(null)
-        done()
-      })
-    })
+        expect(err).to.not.be.equal(null);
+        expect(result).to.be.equal(null);
+        done();
+      });
+    });
 
-     it('avoid creating an existing user', (done)=> {
-      // TODO create this test
-    //   // Warning: the user already exists
-       done()
-     })
-  })
+    it("avoid creating an existing user", (done) => {
+      //   // TODO create this test
+      //   // Warning: the user already exists
+
+      const user = {
+        username: "lilparenzi",
+        firstname: "Léo",
+        lastname: "Parrent",
+      };
+      userController.create(user, () => {
+
+        userController.create(user, (err,result) => {
+
+        expect(err).to.not.be.equal(null);
+        expect(result).to.be.equal(null);
+        done();
+        })
+      });
+
+    
+    });
+  });
 
   // TODO Create test for the get method
-  describe('Get', ()=> {
-   
-    it('get a user by username', (done) => {
+  describe("Get", () => {
+    it("get a user by username", (done) => {
       //1. First, create a user to make this unit test independent from the others
       const user = {
-        username: 'tomchopin',
-        firstname: 'Thomas',
-        lastname: 'Chopin'
-      }
+        username: "tomchopin",
+        firstname: "Thomas",
+        lastname: "Chopin",
+      };
 
-      // 2. Then, check if the result of the get method is correct
-      db.get(user ,(username,firstname,lastname) =>  {
-        console.log(username)
-        console.log(firstname)
-        console.log(lastname)
-      })
+      userController.create(user, () => {
+        userController.get(user.username, (err, result) => {
+          expect(err).to.be.equal(null);
+          expect(result).to.be.deep.equal({
+            firstname: "Thomas",
+            lastname: "Chopin",
+          });
+          done();
+        });
+      });
+    });
+    
 
-      })
-     it('cannot get a user when it does not exist', (done) => {
-       // Chech with any invalid user
-       done()
-     })
-  
-   })
-})
+    it("cannot get a user when it does not exist", (done) => {
+      // Check si l'utilisateur est valide
+      userController.get("invalid", (err, result) => {
+        expect(err).to.not.be.equal(null);
+        expect(result).to.be.equal(null);
+      });
+      done();
+    });
+  });
+});
--- test/user.router.js
+++ test/user.router.js
@@ -1,65 +1,109 @@
-const app = require('../src/index')
-const chai = require('chai')
-const chaiHttp = require('chai-http')
-const db = require('../src/dbClient')
+const app = require("../src/index");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const userController = require("../src/controllers/user");
+const db = require("../src/dbClient");
 
-chai.use(chaiHttp)
+chai.use(chaiHttp);
 
-describe('User REST API', () => {
-  
-    beforeEach(() => {
-      // Clean DB before each test
-      db.flushdb()
-    })
-    
-    after(() => {
-      app.close()
-      db.quit()
-    })
+describe("User REST API", () => {
+  beforeEach(() => {
+    // Clean DB before each test
+    db.flushdb();
+  });
 
-  describe('POST /user', () => {
+  after(() => {
+    app.close();
+    db.quit();
+  });
 
-    it('create a new user', (done) => {
+  describe("POST /user", () => {
+    it("create a new user", (done) => {
       const user = {
-        username: 'sergkudinov',
-        firstname: 'Sergei',
-        lastname: 'Kudinov'
-      }
-      chai.request(app)
-        .post('/user')
+        username: "sergkudinov",
+        firstname: "Sergei",
+        lastname: "Kudinov",
+      };
+      chai
+        .request(app)
+        .post("/user")
         .send(user)
         .then((res) => {
-          chai.expect(res).to.have.status(201)
-          chai.expect(res.body.status).to.equal('success')
-          chai.expect(res).to.be.json
-          done()
+          chai.expect(res).to.have.status(201);
+          chai.expect(res.body.status).to.equal("success");
+          chai.expect(res).to.be.json;
+          done();
         })
         .catch((err) => {
-           throw err
-        })
-    })
-    
-    it('pass wrong parameters', (done) => {
+          throw err;
+        });
+    });
+
+    it("pass wrong parameters", (done) => {
       const user = {
-        firstname: 'Sergei',
-        lastname: 'Kudinov'
-      }
-      chai.request(app)
-        .post('/user')
+        firstname: "Sergei",
+        lastname: "Kudinov",
+      };
+      chai
+        .request(app)
+        .post("/user")
         .send(user)
         .then((res) => {
-          chai.expect(res).to.have.status(400)
-          chai.expect(res.body.status).to.equal('error')
-          chai.expect(res).to.be.json
-          done()
+          chai.expect(res).to.have.status(400);
+          chai.expect(res.body.status).to.equal("error");
+          chai.expect(res).to.be.json;
+          done();
         })
         .catch((err) => {
-           throw err
-        })
-    })
-  })
+          throw err;
+        });
+    });
+  });
 
-  // describe('GET /user', ()=> {
-  //   // TODO Create test for the get method
-  // })
-})
+  describe("GET /user", () => {
+    // TODO Create test for the get method
+    it("create a new user", (done) => {
+      const user = {
+        username: "sergkudinov",
+        firstname: "Sergei",
+        lastname: "Kudinov",
+      };
+
+      userController.create(user, () => {
+        chai
+          .request(app)
+          .get("/user/"+user.username)
+          .then((res) => {
+            chai.expect(res).to.have.status(200);
+            chai.expect(res.body.status).to.equal("Succès");
+            chai.expect(res).to.be.json;
+            done();
+          })
+          .catch((err) => {
+            throw err;
+          });
+      });
+    });
+
+    it("pass wrong parameters", (done) => {
+      const user = {
+        firstname: "Sergei",
+        lastname: "Kudinov",
+      };
+      userController.create(user, () => {
+        chai
+          .request(app)
+          .get("/user/"+user.username)
+          .then((res) => {
+            chai.expect(res).to.have.status(404);
+            chai.expect(res.body.status).to.equal("erreur");
+            chai.expect(res).to.be.json;
+            done();
+          })
+          .catch((err) => {
+            throw err;
+          });
+      });
+    });
+  });
+});
