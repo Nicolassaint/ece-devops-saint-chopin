ec88d456507d941af47aef83042c4dbc82aa306f
thomas91600 <thomas.chopin@edu.ece.fr>
2022-12-26 14:05:57 +0100

    feat: vagrant and ansible

---
+++ src/routes/liveness.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const userController = require("../controllers/user");
+
+const userRouter = express.Router();
+
+userRouter
+  .post("/", (req, resp) => {
+    userController.create(req.body, (err, res) => {
+      let respObj;
+      if (err) {
+        respObj = {
+          status: "error",
+          msg: err.message,
+        };
+        return resp.status(400).json(respObj);
+      }
+      respObj = {
+        status: "success",
+        msg: res,
+      };
+      resp.status(201).json(respObj);
+    });
+  })
+module.exports = userRouter;
